5f99e4f824dfa2a9b8482de8a2ad26c410638e7e
LordPrettyRustyRyan <xp4r7s@gmail.com>
2025-06-13 16:09:20 +0530

    Add files via upload

---
+++ basics/MultiplicationTable.c
@@ -0,0 +1,17 @@
+int main() {
+int a;
+printf("Enter a Number:");
+scanf("%d", &a);
+
+printf("%d\n", a*1);
+printf("%d\n", a*2);
+printf("%d\n", a*3);
+printf("%d\n", a*4);
+printf("%d\n", a*5);
+printf("%d\n", a*6);
+printf("%d\n", a*7);
+printf("%d\n", a*8);
+printf("%d\n", a*9);
+printf("%d\n", a*10);
+
+}
