e297c295019b9f3eded516159dbc1f595faeff58
LordPrettyRustyRyan <xp4r7s@gmail.com>
2025-06-18 01:07:14 +0530

    Update youpassed.c

--- basics/youpassed.c
+++ basics/youpassed.c
@@ -2,14 +2,17 @@
 
 int main() {
 char c;
+
 printf("So which exams you passed?\nChoose options (A, B or C):\nA.Maths\nB.English\nC.Both\n\n");
 scanf(" %c", &c);
+
+printf("\nif-else-if output:");
 
 if (c=='A'||c=='a'){
     printf("\nCongrats, you Passed Maths \n Here's you gift: \n $15");
 }
 else if (c=='B'||c=='b'){
-    printf("\nCongrats, you Passed Englsih \n Here's you gift: \n $15");
+    printf("\nCongrats, you Passed English \n Here's you gift: \n $15");
 }
 else if (c=='C'||c=='c'){
     printf("\nCongrats, you Passed Both exams \n Here's you gift: \n $45");
@@ -21,4 +24,25 @@ else {
 //You should use == for comparison, not =
 //= is assignment | == is comparison
 //'||' is OR operator
+
+printf("\n\nswitch output:");
+
+switch (c){
+    case 'A':
+    case 'a':
+    printf("\nCongrats, you Passed Maths \n Here's you gift: \n $15");
+    break;
+    case 'B':
+    case 'b':
+    printf("\nCongrats, you Passed English \n Here's you gift: \n $15");
+    break;
+    case 'C':
+    case 'c':
+    printf("\nCongrats, you Passed both \n Here's you gift: \n $45");
+    break;
+    default:
+    printf("\nman,get outta here!");
+}
+
+//next step be using toupper() instead of using double cases.
 }
